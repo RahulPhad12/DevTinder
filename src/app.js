47db1e80fe72e55ecd1fbe83ea3c6bb1e4233392
Rahul Phad <rahulphad225@gmail.com>
2026-04-19 17:08:04 +0530

    API Call

--- src/app.js
+++ src/app.js
@@ -6,10 +6,17 @@ app.use("/test",(req, res) => {
   res.send("Hello, World!");
 });
 
-app.use("/",(req, res) => {
-  res.send("Hello Dashboard!");
+// app.use("/",(req, res) => {
+//   res.send("Hello Dashboard!");
+// });
+
+app.get("/user",(req, res) => {
+  res.send({"first":"Hello User!", "second":"Welcome to DevTinder!"});
 });
 
+app.post("/user",(req, res) => {
+  res.send("Data received successfully!");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
